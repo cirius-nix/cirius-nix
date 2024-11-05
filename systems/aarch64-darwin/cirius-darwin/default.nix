d74130ec176f11fc6e7da30083684f5d548e0ed2
Minh Hieu Tran <hieu.tran21198@gmail.com>
2024-11-05 11:24:24 +0700

    enabled virtualisation

--- systems/aarch64-darwin/cirius-darwin/default.nix
+++ systems/aarch64-darwin/cirius-darwin/default.nix
@@ -15,6 +15,8 @@
 
     # enable modules defined in: pkg_root/modules/darwin
     home-manager.enable = true;
+    nix.enable = true;
+    virtualisation.enable = true;
   };
 
   nix.settings.experimental-features = "nix-command flakes";
