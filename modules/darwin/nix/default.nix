00a1b59147d2090f14a93391973e220c9b8d8cc7
Minh Hieu Tran <hieu.tran21198@gmail.com>
2024-11-05 12:57:43 +0700

    feat(darwin-nix): added nix config

---
+++ modules/darwin/nix/default.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.cirius.nix;
+in
+{
+  options.cirius.nix = {
+    enable = mkEnableOption "Nix";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      statix
+      deadnix
+      nixfmt-rfc-style
+      nixpkgs-fmt
+      nixd
+    ];
+  };
+}
