ea588435db99d3ae7a13d5335f8f2347d6c3a6d7
Minh Hieu Tran <hieu.tran21198@gmail.com>
2024-11-22 09:06:12 +0700

    feat(gpu): added nvidia setting

--- systems/x86_64-linux/cirius/default.nix
+++ systems/x86_64-linux/cirius/default.nix
@@ -29,6 +29,9 @@
     input-method.enable = true;
     bluetooth.enable = true;
     appimage.enable = true;
+    gpu = {
+      nvidia.enable = true;
+    };
   };
 
   boot.loader = {
@@ -38,40 +41,6 @@
 
   services = {
     printing.enable = false;
-  };
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = true;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-  };
-
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.graphics = {
-    enable = true;
   };
 
   nixpkgs.config.allowUnfree = true;
